28a894f230ea7aa058fe05d022d695a399982c6d
alex mitre <bedr10_capacitacion@hotmail.com>
2025-07-28 20:53:39 -0600

    Initial Commit

---
+++ sicopa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sicopa
+//
+//  Created by Dr. Alex Mitre on 28/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sicopa/sicopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sicopaApp.swift
+//  sicopa
+//
+//  Created by Dr. Alex Mitre on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct sicopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
